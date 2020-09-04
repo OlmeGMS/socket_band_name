cb81b264dd4e18518e51efe1c5e97fc39686219d
olme marin <olme_gms@hotmail.com>
2020-09-04 16:45:21 -0500

    configuracion servidor

---
+++ sockets/socket.js
@@ -0,0 +1,14 @@
+const {io} = require('../index');
+
+//Mensajes de sockecs
+io.on('connection', client => {
+    console.log('cliente conectado !!');
+    client.on('disconnect', () => { 
+        console.log('cliente desconectado !!');
+    });
+
+    client.on('mensaje', (payload) => {
+        console.log('Mensaje !! ', payload);
+        io.emit('mensaje', {admin: 'Nuevo Mensaje'});
+    });
+});
